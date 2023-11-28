36be1184be7b44ecb2729cadebab62860b83ab5d
Himanshu Shukla <himanshushukla.shukla3@gmail.com>
2023-11-28 22:45:03 +0530

    fixed the code

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,8 +4,7 @@ public class Calculator {
     public int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
-        }
-        if(numbers.length()==1){
+        }else if(numbers.length()==1){
             if(numbers.matches(".*[^a-zA-Z0-9].*")){
                 throw new IllegalArgumentException("Invalid input: Single hyphen is not allowed");
             }
