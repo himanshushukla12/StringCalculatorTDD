905a4871d8f0793f9f612573092462af4b6ffa36
Himanshu Shukla <himanshushukla.shukla3@gmail.com>
2023-11-28 22:22:24 +0530

    Function for test on empty string is added and passed the test case

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Calculator {
+    public int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return 0;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,16 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    @Test
+    @DisplayName("Test on Empty String")
+    public void testAddEmptyString() {
+        Calculator calculator=new Calculator();
+        int expected=0;
+        int actual=calculator.add("");
+        assertEquals(expected,actual);
+    }
 }
